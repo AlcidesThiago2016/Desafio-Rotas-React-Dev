1c14efe51b36f3d5cf3ff5e2fd027edebd76bf36
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-02-20 15:29:03 -0300

    Create Route Computers

--- desafioRotasReactDev/src/App.tsx
+++ desafioRotasReactDev/src/App.tsx
@@ -2,6 +2,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Home from './routes/Home'
 import HomeBody from './routes/Home/HomeBody'
+import Products from './routes/Home/Products'
+import Computers from './routes/Home/Products/Computers'
 
 function App() {
  
@@ -11,9 +13,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}>
-          <Route index element={<Navigate to="/home" />} />
-          <Route path="home" element={<HomeBody />} />
-          /</Route>
+            <Route index element={<Navigate to="/home" />} />
+            <Route path="home" element={<HomeBody />} />
+            <Route path="products" element={<Products />}>
+              <Route path="computers" element={<Computers />} />
+            </Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ desafioRotasReactDev/src/routes/Home/Products/Computers/index.tsx
@@ -0,0 +1,9 @@
+export default function Computers(){
+    return(
+        <section className="mt20 content">
+            <p>Computador Dell</p>
+            <p>Computador Asus</p>
+            <p>Computador Apple</p>
+        </section>
+    );
+}
