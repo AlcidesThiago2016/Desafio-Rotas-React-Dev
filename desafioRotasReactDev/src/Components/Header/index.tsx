e749f90300e01218f35afdf9a13a32834bedcc8a
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-02-20 14:34:55 -0300

    Create component Header

---
+++ desafioRotasReactDev/src/Components/Header/index.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./styles.css";
+import iconHome from "../../assets/home.svg";
+
+export default function Header(){
+    return(
+        <header>
+            <nav className="container">
+                <div className="nav-left">
+                    <div>
+                        <NavLink to="/home" className={({ isActive}) => isActive ? "active-menu-item" : ""}
+                        >
+                            Inicio
+                        </NavLink>
+                    </div>
+                    <div>
+                        {" "}
+                        <NavLink to="/products" className={({ isActive}) => isActive ? "active-menu-item" : ""}
+                        >
+                            Produtos
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="/about" className={({ isActive}) => isActive ? "active-menu-item" : ""}
+                        >
+                            Sobre nós
+                        </NavLink>
+                    </div>
+                    <div className="nav-icon">
+                        <Link to="/">
+                            {" "}
+                            <img src={iconHome} alt="Inicio" />
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
